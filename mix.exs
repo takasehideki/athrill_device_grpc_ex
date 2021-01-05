a7ff2a16e53cbaac326c9b6aaeea9c8479e8ed5b
takasehideki <takase@i.kyoto-u.ac.jp>
2021-01-05 11:36:18 +0900

    add SampleServerApp module to invoke when the application started

--- mix.exs
+++ mix.exs
@@ -14,6 +14,7 @@ defmodule AthrillDeviceGrpcEx.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
+      mod: {SampleServerApp, []},
       extra_applications: [:logger]
     ]
   end
